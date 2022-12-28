4e2a06dafb021c40a25e6c26e8a5c717e1f905eb
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-12-28 17:18:12 +0700

    Declaring info, left child, and right child

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Node
+    {
+        public string info;
+        public Node leftchild;
+        public Node rightchild;
+
+        //constructor for the node class
+
+        public Node(string i, Node l, Node r)
+        {
+            info = i;
+            leftchild = l;
+            rightchild = r;
+        }
+    }
+
+    /*a node class consist of three things, the information,
+     * references to the right child, and references to the left child.*/
+
+    
+}
