951450c8124e43b25d2f3663abb1cc3751a2fcbb
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-12-28 17:25:43 +0700

    Create a method for inserting a node to the binary search tree

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -44,5 +44,33 @@ namespace BinarySearchTree
                     currentNode = currentNode.rightchild;
             }
         }
+
+    public void insert(string element)/*Insert a node in the binary search tree*/
+        {
+            Node tmp, parent = null, currentNode = null;
+            search(element, ref parent, ref currentNode);
+            if (currentNode != null)/*Check if the node to be inserted already
+                                     inserted or not*/
+            {
+                Console.WriteLine("Duplicate words are not allowed");
+                return;
+            }
+            else /*if the specified node is not present*/
+            {
+                tmp = new Node(element, null, null); /*creates a node*/
+                if (parent == null) /* if the tree is empty*/
+                {
+                    ROOT = tmp;
+                }
+                else if (string.Compare(element, parent.info) < 0)
+                {
+                    parent.leftchild = tmp;
+                }
+                else
+                {
+                    parent.rightchild = tmp;
+                }
+            }
+        }
     }
 }
