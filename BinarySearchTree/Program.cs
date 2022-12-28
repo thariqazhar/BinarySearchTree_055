6381281d32c88cbc21adfcb60d3accad82e0070d
TC1055 <thariq.azhar.ft21@mail.umy.ac.id>
2022-12-28 17:58:19 +0700

    Create a main menu

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BinarySearchTree
 {
@@ -82,7 +86,7 @@ namespace BinarySearchTree
             if (ptr != null)
             {
                 inorder(ptr.leftchild);
-                Console.WriteLine(ptr.info + "");
+                Console.Write(ptr.info + "  ");
                 inorder(ptr.rightchild);
             }
         }
@@ -95,7 +99,7 @@ namespace BinarySearchTree
             }
             if (ptr != null)
             {
-                Console.WriteLine(ptr.info + "");
+                Console.Write(ptr.info + "  ");
                 preorder(ptr.leftchild);
                 preorder(ptr.rightchild);
             }
@@ -111,7 +115,55 @@ namespace BinarySearchTree
             {
                 postorder(ptr.leftchild);
                 postorder(ptr.rightchild);
-                Console.WriteLine(ptr.info + "");
+                Console.Write(ptr.info + "  ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            program x = new program();
+            while (true)
+            {
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("1. Implementasi insert option");
+                Console.WriteLine("2. Perform inorder traversal");
+                Console.WriteLine("3. Perform preorder traversal");
+                Console.WriteLine("4. Perform postorder traversal");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("\nEnter your choice (1-5) :");
+                char ch = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine();
+                switch (ch)
+                {
+                    case '1':
+                        {
+                            Console.Write("Enter a word: ");
+                            string word = Console.ReadLine();
+                            x.insert(word);
+                        }
+                        break;
+                    case '2':
+                        {
+                            x.inorder(x.ROOT);
+                        }
+                        break;
+                    case '3':
+                        {
+                            x.preorder(x.ROOT);
+                        }
+                        break;
+                    case '4':
+                        {
+                            x.postorder(x.ROOT);
+                        }
+                        break;
+                    case '5':
+                        return;
+                    default:
+                        {
+                            Console.WriteLine("Invalid option");
+                            break;
+                        }
+                }
             }
         }
     }
